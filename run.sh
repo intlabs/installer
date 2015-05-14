e92427cf8d6eb4d11f352add1cce746879973d9e
Pete Birley <pete@cannycomputing.com>
2015-05-14 10:19:45 +0100

    initial cannyos

--- run.sh
+++ run.sh
@@ -9,13 +9,17 @@ export IMAGES_PORT=8014
 docker run -d -p $REPO_PORT:80 cannyos/atomic_dist_rpmostree
 docker run -d -p $INSTALLER_PORT:80 cannyos/atomic_dist_installer:stable
 
-export SSH_PUBLIC_KEY=$(echo $(cat /root/.ssh/id_rsa.pub))
+
+
 git stash && git pull && \
 docker build -t "cannyos/installer" $(pwd) && \
+SWARM_TOKEN=$( docker run --rm swarm create ) && \
+SSH_PUBLIC_KEY=$(echo $(cat /root/.ssh/id_rsa.pub)) && \
 docker run -it --rm --privileged \
 --net=host \
 -e INTERFACE=eth1 \
 -e SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" \
 -e REPO_PORT="$REPO_PORT" \
 -e INSTALLER_PORT="$INSTALLER_PORT" \
+-e SWARM_TOKEN="$SWARM_TOKEN" \
 cannyos/installer
