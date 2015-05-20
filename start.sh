1d04d6ba9ab2e12f8a48ef96beb9a3761899f42f
Pete Birley <petebirley@gmail.com>
2015-05-20 13:09:53 +0100

    update start

--- start.sh
+++ start.sh
@@ -1,13 +1,27 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-pxe_server_ip=$( ip -f inet -o addr show $INTERFACE | cut -d\  -f 7 | cut -d/ -f 1 )
 
-echo Server IP: $pxe_server_ip
+echo Setting up iptables... && \
+iptables -t nat -A POSTROUTING -j MASQUERADE
+
+
+echo Waiting for pipework to give us the eth1 interface... && \
+/bin/pipework --wait
+
+INTERFACE=eth1
+
+    
+ROUTER_IP=$( ip -f inet -o addr show $INTERFACE | cut -d\  -f 7 | cut -d/ -f 1 )
+echo Server IP: $ROUTER_IP
+
+
+    
+# Fix our cloudconfig folder for now
 mkdir -p /usr/share/nginx/html/cloudconfig
 touch /usr/share/nginx/html/cloudconfig/test
-#/bin/bash
+
+# Install the cannyos kernel and ramdisk
 mkdir -p $TFTP_BOOT/cannyos
 curl http://$pxe_server_ip:$INSTALLER_PORT/installer/lorax/images/pxeboot/initrd.img > $TFTP_BOOT/cannyos/initrd.img
 curl http://$pxe_server_ip:$INSTALLER_PORT/installer/lorax/images/pxeboot/upgrade.img > $TFTP_BOOT/cannyos/upgrade.img
@@ -19,11 +33,20 @@ sed -i "s/%(server_ip)s/$pxe_server_ip/g" /usr/share/nginx/html/ks/*
 sed -i "s/{{ SERVER_IP }}/$pxe_server_ip/g" /usr/share/nginx/html/cloudconfig/*
 
 
-
-
 SSH_PUBLIC_KEY_SAFE=$(echo $SSH_PUBLIC_KEY | sed "s,/,\\\/,g")
-
 sed -i "s/{{ SSH_PUBLIC_KEY }}/$SSH_PUBLIC_KEY_SAFE/g" /usr/share/nginx/html/ks/*
+
+
+echo "ETCD discovery token: $ETCD_DISCOVERY_TOKEN"
+sed -i "s,{{ ETCD_DISCOVERY_TOKEN }},$ETCD_DISCOVERY_TOKEN,g" /usr/share/nginx/html/ks/*
+sed -i "s,{{ ETCD_DISCOVERY_TOKEN }},$ETCD_DISCOVERY_TOKEN,g" /usr/share/nginx/html/cloudconfig/*
+
+
+
+echo "SWARM discovery token: $SWARM_TOKEN"
+sed -i "s,{{ SWARM_TOKEN }},$SWARM_TOKEN,g" /usr/share/nginx/html/ks/*
+sed -i "s,{{ SWARM_TOKEN }},$SWARM_TOKEN,g" /usr/share/nginx/html/cloudconfig/*
+
 
 
 sed -i "s/{{ REPO_PORT }}/$REPO_PORT/g" /var/lib/tftpboot/pxelinux.cfg/default
@@ -39,31 +62,18 @@ sed -i "s/{{ IMAGES_PORT }}/$IMAGES_PORT/g" /usr/share/nginx/html/ks/*
 sed -i "s/{{ IMAGES_PORT }}/$IMAGES_PORT/g" /usr/share/nginx/html/cloudconfig/*
 
 
-
-echo "ETCD discovery token: $ETCD_DISCOVERY_TOKEN"
-sed -i "s,{{ ETCD_DISCOVERY_TOKEN }},$ETCD_DISCOVERY_TOKEN,g" /usr/share/nginx/html/ks/*
-sed -i "s,{{ ETCD_DISCOVERY_TOKEN }},$ETCD_DISCOVERY_TOKEN,g" /usr/share/nginx/html/cloudconfig/*
-
-
-
-
-echo "SWARM discovery token: $SWARM_TOKEN"
-sed -i "s,{{ SWARM_TOKEN }},$SWARM_TOKEN,g" /usr/share/nginx/html/ks/*
-sed -i "s,{{ SWARM_TOKEN }},$SWARM_TOKEN,g" /usr/share/nginx/html/cloudconfig/*
-
-
-
-
 nginx
 
 cat /var/lib/tftpboot/pxelinux.cfg/default
 
-pxe_server_ip=$( ip -f inet -o addr show $INTERFACE | cut -d\  -f 7 | cut -d/ -f 1 )
 
+
+# Launching dnsmasq
 dnsmasq \
-    --dhcp-range=$INTERFACE,$DHCP_START,$DHCP_END,$DHCP_NETMASK \
-    --dhcp-option=option:router,$pxe_server_ip \
-    --dhcp-boot=pxelinux.0,pxeserver,$pxe_server_ip \
+    --interface=eth1
+    --dhcp-range=$DHCP_START,$DHCP_END,$DHCP_NETMASK,1h \
+    --dhcp-option=option:router,$ROUTER_IP \
+    --dhcp-boot=pxelinux.0,pxeserver,$ROUTER_IP \
     --pxe-service=x86PC,"CannyOS: Managed Boot",pxelinux \
     --enable-tftp \
     --tftp-root=/var/lib/tftpboot \
